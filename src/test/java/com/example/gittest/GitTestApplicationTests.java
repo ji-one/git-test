fa5b9bc3c85f6b1603f2dd8fe56b7b51f4579286
ji-one <rlawldnjs8@gmail.com>
2023-02-08 23:36:03 +0900

    test: access key 추가

--- src/test/java/com/example/gittest/GitTestApplicationTests.java
+++ src/test/java/com/example/gittest/GitTestApplicationTests.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GitTestApplicationTests {
 
+	private static final String accessKey = "***REMOVED***";
+
 	@Test
 	void contextLoads() {
 	}
